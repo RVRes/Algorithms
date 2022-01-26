f6aa02c3f090c2e68011a7b072767104185d1c14
scorpion <rv.reshetnikov@gmail.com>
2022-01-25 18:32:00 -0600

    added Integral solver

---
+++ integral.py
@@ -0,0 +1,20 @@
+import math as m
+
+INTEGRALF = lambda x: x**2*m.sin(x)
+ACCURACY = 10000000
+LIMIT_LOW = 0
+LIMIT_HI = 10
+
+
+def integral_count(fnct: callable, acc: int, l_hi: int, l_lo: int) -> float:
+    result = 0
+    step = (l_hi - l_lo) / acc
+    cur_point = l_lo
+    l_hi -= step
+    while cur_point < l_hi:
+        result += fnct(cur_point)*step
+        cur_point += step
+    return round(result, 6)
+
+
+print(integral_count(INTEGRALF, ACCURACY, LIMIT_HI, LIMIT_LOW))
