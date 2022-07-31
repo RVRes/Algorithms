5721288a19ce1c25ea8e1d64ddc0c573c9b1fbf9
scorpion <rv.reshetnikov@gmail.com>
2022-07-31 01:31:19 -0500

    Added sorts.ipynb - example of multifactor sort. Added operator_module.ipynb - example of operator.itemgetter. Moved strings.py to strings.ipynb.

--- strings.py
+++
@@ -1,4 +0,0 @@
-# strings could be encoded or decoded with standard library translate method
-source, dest, decoded, encoded = ('abcd', '*d%#', 'abacabadaba', '#*%*d*%')
-print(decoded.translate(str.maketrans(source, dest)))
-print(encoded.translate(str.maketrans(dest, source)))
